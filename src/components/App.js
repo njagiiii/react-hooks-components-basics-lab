5e1ade9d97422dc742d9bee0b54c01f7c8de2310
njagiiii <grace.makena@student.moringaschool.com>
2023-07-19 22:01:01 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -23,8 +23,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
